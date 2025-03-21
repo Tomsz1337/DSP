a64370080cfd6c9dc1290386fbb4179a51527d7d
tomasz ochmanek <126971464+Tomsz1337@users.noreply.github.com>
2025-03-21 16:48:24 +0100

    Update iir.c

--- lab1/iir.c
+++ lab1/iir.c
@@ -1,88 +1,64 @@
+#include "fir.h"
+#include "qCode.h"
+#define NOF_SAMPLES 16
+
+const float fir_coeff[NOF_SAMPLES]  = {0.034, 0.074, 0.0188, 0.0395, 0.0677, 0.0984, 0.1248, 0.1400, 0.1400, 0.1248, 0.0984, 0.0677, 0.0395, 0.0188, 0.074, 0.034};
+const float firQ_coeff[NOF_SAMPLES] = {112,   243,   618,    1293,   2217,   3225,   4089,   4587,   4587,   4089,   3225,   2217,   1293,   618,    243,   112  };
+
+float circural_buff[NOF_SAMPLES];
+float circural_Qbuff[NOF_SAMPLES];
+
+static void bufferStep(float new_sample){
+    uint8_t sampleCtr;
+    for(sampleCtr = NOF_SAMPLES; sampleCtr > 0; sampleCtr--)
+        circural_buff[sampleCtr] = circural_buff[sampleCtr-1];
+
+    circural_buff[0] = new_sample;
+}
+
 /*
- * iir.c
- *
- *  Created on: Mar 21, 2025
- *      Author: student
- */
-
-#include "iir.h"
-#include <stdint.h>
-#include <stdio.h>
-#define NOF_SAMPLES_IIR 3
-
-const int32_t 	iirQ_coeff_num[3]  = {2029, 4058, 2029};	//Q1.15
-const int32_t 	iirQ_coeff_den[3] = {16384, -20732, 10080}; //Q2.14
-
-int32_t cbuff[3];
-int32_t cbuff2[3];
-
-static void bufferStep(int32_t new_sample)
-{
+    FIR_filter:
+    Calculates one FIR output and gets one input
+*/
+float FIR_filter(float data){
+    float Accumulator = 0;
     uint8_t sampleCtr;
-    for(sampleCtr = NOF_SAMPLES_IIR; sampleCtr > 0; sampleCtr--)
-        cbuff[sampleCtr] = cbuff[sampleCtr-1];
 
-    cbuff[0] = new_sample;
-}
-
-static void bufferStep2(int32_t new_sample)
-{
-    uint8_t sampleCtr;
-    for(sampleCtr = NOF_SAMPLES_IIR; sampleCtr > 0; sampleCtr--)
-        cbuff2[sampleCtr] = cbuff2[sampleCtr-1];
-
-    cbuff2[0] = new_sample;
-}
-
-void IIR_Init(void){
-    uint8_t sampleCtr;
-    for(sampleCtr = 0; sampleCtr < NOF_SAMPLES_IIR; sampleCtr++){
-        cbuff[sampleCtr] = 0;
-        cbuff2[sampleCtr] = 0;
-    }
-}
-int32_t IIR_filter_TOP(int32_t data)
-{
-    int32_t acc = 0;
-    int32_t reg = 0;
-    uint8_t sampleCtr;
-    
     bufferStep(data);
 
-    for(sampleCtr = 0; sampleCtr < NOF_SAMPLES_IIR; sampleCtr++)
-    {
-        acc += (int32_t)cbuff[sampleCtr] * iirQ_coeff_num[sampleCtr];
-    }
-    printf("wynik dla licznika: %ld\n", acc);
-    for(sampleCtr = 0; sampleCtr < NOF_SAMPLES_IIR; sampleCtr++)  
-    {
-    	reg += cbuff[sampleCtr] * iirQ_coeff_den[sampleCtr]; 
-    }
-    printf("wynik dla mianownika: %ld\n", reg);
-    	
-    
-    return acc + reg;
-}
-int32_t IIR_filter_BOT(int32_t data)
-{
-	int32_t acc = 0;
-	uint8_t sampleCtr; 
-	
-	bufferStep2(data);
-	
-	for(sampleCtr = 0; sampleCtr < NOF_SAMPLES_IIR; sampleCtr++)  
-	{
-		acc += cbuff2[sampleCtr] * iirQ_coeff_den[sampleCtr]; 
-	}
-	printf("wynik dla mianownika: %ld\n", acc);
-	return acc;
+    for(sampleCtr = 0; sampleCtr < NOF_SAMPLES; sampleCtr++)
+        Accumulator += circural_buff[sampleCtr] * fir_coeff[sampleCtr];
+
+    return Accumulator;
 }
 
-int32_t IIR(int16_t data)
-{
-	int32_t result1;
-	int32_t result2;
-	result1 = IIR_filter_TOP(data);
-	result2 = IIR_filter_BOT(result1);
-	return result1 + result2;
+/*
+    FIR_Init:
+    Clears circural buffer
+*/
+void FIR_Init(void){
+    uint8_t sampleCtr;
+    for(sampleCtr = 0; sampleCtr < NOF_SAMPLES; sampleCtr++){
+        circural_buff[sampleCtr] = 0;
+    }
+}
+
+
+/*
+    FIRQ_filter:
+    Calculates one FIR output and gets one input
+*/
+int32_t FIRQ_filter(int32_t data){
+    float Accumulator = 0;
+    uint8_t sampleCtr;
+
+    bufferStep(data);
+
+    for(sampleCtr = 0; sampleCtr < NOF_SAMPLES; sampleCtr++)
+    {
+    	Accumulator += circural_buff[sampleCtr] * firQ_coeff[sampleCtr];
+    }
+        
+
+    return Accumulator;
 }
