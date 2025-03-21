e93d63526dab2b4ac5d851ba0d43ec7325ef9c22
tomasz ochmanek <126971464+Tomsz1337@users.noreply.github.com>
2025-03-21 16:48:53 +0100

    Update fir.h

--- lab1/fir.h
+++ lab1/fir.h
@@ -1,16 +1,15 @@
 /*
  * fir.h
  *
- *  Created on: Mar 20, 2025
- *      Author: tomas
+ *  Created on: Mar 21, 2025
+ *      Author: student
  */
 
 #ifndef FIR_H_
 #define FIR_H_
-
 #include <stdint.h>
 
 float FIR_filter(float data);
 void FIR_Init(void);
-
+int32_t FIRQ_filter(int32_t data);
 #endif /* FIR_H_ */
